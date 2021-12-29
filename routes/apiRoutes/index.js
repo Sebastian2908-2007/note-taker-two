334c61b4cbed9bbd5a3e3b75bd968655105018b8
Sebabin <sebastiancrespin279@gmail.com>
2021-12-28 17:22:32 -0700

    removing note with proper id and returning a new array

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {notes} = require('../../db/db.json');
-const {getNotes, createNewNote} = require('../../lib/notes');
+const {getNotes, createNewNote,deleteNote} = require('../../lib/notes');
 const uniquid = require('uniqid');
 
 router.get('/notes', (req,res) => {
@@ -13,6 +13,16 @@ router.post('/notes', (req,res) => {
     req.body.id = uniquid();
     const note = createNewNote(req.body,notes);
     res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const noteToDelete = req.params;
+    if(!noteToDelete) {
+       return res.sendStatus(404)
+    }
+    
+    deleteNote(noteToDelete,notes);
+
 });
 
 module.exports = router;
